6887a77a086c0e4d0549f56c7cbb79732634ddb9
Ratmir Gaitov <gaitovrat@gmail.com>
2021-10-03 16:33:19 +0200

    #1: Complete the task

--- 1-4/process.cpp
+++ 1-4/process.cpp
@@ -11,15 +11,14 @@
 #define BUFFER_SIZE 1024
 
 int createProcess();
+void createPipe(int *t_pipe);
 
 int main(void)
 {
-    int l_pipe[2];
-    if (pipe(l_pipe) < 0)
-    {
-        log(error, "Unable to create pipe!");
-        return 1;
-    }
+    int l_pipeFirst[2];
+    int l_pipeSecond[2];
+    createPipe(l_pipeFirst);
+    createPipe(l_pipeSecond);
 
     int l_processFirst = createProcess();
     int l_processSecond = createProcess();
@@ -27,51 +26,83 @@ int main(void)
     /* First child */
     if (l_processFirst == 0 && l_processSecond != 0)
     {
-        close(l_pipe[0]);
+        close(l_pipeFirst[0]);
+        for (int i = 0; i < 2; ++i)
+            close(l_pipeSecond[i]);
 
-        char buffer[BUFFER_SIZE];
-
-        int number = randomNumber();
-        sprintf(buffer, "%d", number);
-        
-        if (write(l_pipe[1], buffer, BUFFER_SIZE) < 0)
+        while (1)
         {
-            log(error, "First child unable to write to pipe!");
-            return 1;
+            char buffer[BUFFER_SIZE];
+
+            int number = randomNumber();
+            sprintf(buffer, "%d", number);
+            if (write(l_pipeFirst[1], buffer, BUFFER_SIZE) < 0)
+            {
+                log(error, "First child unable to write to first pipe!");
+                continue;
+            }
         }
-
-        log(info, "First child write random number.");
-
-        close(l_pipe[1]);
+        close(l_pipeFirst[1]);
     } 
+    /* Second child */
     if (l_processSecond == 0 && l_processFirst != 0)
     {
-        close(l_pipe[1]);
+        close(l_pipeFirst[1]);
+        close(l_pipeSecond[0]);
 
+        int sum = 0;
         char buffer[BUFFER_SIZE];
-        int sum = 10;
 
-        if (read(l_pipe[0], buffer, BUFFER_SIZE) < 0)
+        while (1)
         {
-            log(error, "Second child unable to read form pipe!");
-            return 1;
+            if (read(l_pipeFirst[0], buffer, BUFFER_SIZE) < 0)
+            {
+                log(error, "Second child unable to read from first pipe!");
+                continue;
+            }
+
+            sum += atoi(buffer);
+
+            memset(buffer, 0, BUFFER_SIZE);
+            sprintf(buffer, "%d", sum);
+            if (write(l_pipeSecond[1], buffer, BUFFER_SIZE) < 0)
+            {
+                log(error, "Second child unable to write to second pipe!");
+            }
         }
 
-        sum += atoi(buffer);
-        memset(buffer, 0, BUFFER_SIZE);
-        sprintf(buffer, "Second child sum: %d", sum);
-        log(info, "Second child read number from first child");
-        log(info, buffer);
-
-        close(l_pipe[0]);
+        close(l_pipeFirst[0]);
+        close(l_pipeSecond[1]);
     } 
+    /* Parent */
     if(l_processFirst > 0 || l_processSecond > 0)
     {
         for (int i = 0; i < 2; ++i)
+            close(l_pipeFirst[i]);
+        close(l_pipeSecond[1]);
+
+        char buffer[BUFFER_SIZE];
+        int count = 0;
+        int sum = 0;
+
+        while (1)
         {
-            close(l_pipe[i]);
-            wait(NULL);
+            if (read(l_pipeSecond[0], buffer, BUFFER_SIZE) < 0)
+            {
+                log(error, "Parent unable to read from second pipe!");
+                continue;
+            }
+
+            sum = atoi(buffer);
+            count++;
+
+            memset(buffer, 0, BUFFER_SIZE);
+            sprintf(buffer, "%d %d %f", sum, count, sum / static_cast<float>(count));
+
+            log(info, buffer);
         }
+
+        close(l_pipeSecond[0]);
     }
 
     return 0;
@@ -87,4 +118,13 @@ int createProcess()
     }
 
     return l_process;
+}
+
+void createPipe(int *t_pipe)
+{
+    if (pipe(t_pipe) < 0)
+    {
+        log(error, "Unable to create pipe!");
+        exit(1);
+    }
 }
