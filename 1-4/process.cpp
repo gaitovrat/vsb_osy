4bff36469829f5e2aae936f7c3ab8724afeeed42
Ratmir Gaitov <gaitovrat@gmail.com>
2021-10-03 16:05:34 +0200

    #1: Complete second part of the task

--- 1-4/process.cpp
+++ 1-4/process.cpp
@@ -1,12 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <string.h>
 
 #include "log.h"
 #include "random.h"
 
 #define BUFFER_SIZE 1024
+
+int createProcess();
 
 int main(void)
 {
@@ -17,42 +21,70 @@ int main(void)
         return 1;
     }
 
-    int l_process;
-    if ((l_process = fork()) < 0)
-    {
-        log(error, "Unable to fork a child process");
-        return 1;
-    }
+    int l_processFirst = createProcess();
+    int l_processSecond = createProcess();
 
-    /* Parent process */
-    if (l_process > 0)
+    /* First child */
+    if (l_processFirst == 0 && l_processSecond != 0)
     {
+        close(l_pipe[0]);
+
         char buffer[BUFFER_SIZE];
 
         int number = randomNumber();
-        sprintf(buffer, "Number from parent: %d\n", number);
-
+        sprintf(buffer, "%d", number);
+        
         if (write(l_pipe[1], buffer, BUFFER_SIZE) < 0)
         {
-            log(error, "Parent: Unable to write to pipe!");
+            log(error, "First child unable to write to pipe!");
             return 1;
-        } else
-            log(info, "Parent: writes to pipe.");
-    }
-    /* Child process */
-    else
+        }
+
+        log(info, "First child write random number.");
+
+        close(l_pipe[1]);
+    } 
+    if (l_processSecond == 0 && l_processFirst != 0)
     {
+        close(l_pipe[1]);
+
         char buffer[BUFFER_SIZE];
+        int sum = 10;
 
         if (read(l_pipe[0], buffer, BUFFER_SIZE) < 0)
         {
-            log(error, "Child: Unable to read from pipe!");
-        } else
+            log(error, "Second child unable to read form pipe!");
+            return 1;
+        }
+
+        sum += atoi(buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+        sprintf(buffer, "Second child sum: %d", sum);
+        log(info, "Second child read number from first child");
+        log(info, buffer);
+
+        close(l_pipe[0]);
+    } 
+    if(l_processFirst > 0 || l_processSecond > 0)
+    {
+        for (int i = 0; i < 2; ++i)
         {
-            log(info, "Child: Read successfully from pipe.");
-            log(info, buffer);
+            close(l_pipe[i]);
+            wait(NULL);
         }
     }
 
     return 0;
+}
+
+int createProcess()
+{
+    int l_process;
+    if ((l_process = fork()) < 0)
+    {
+        log(error, "Unable to fork a child process");
+        exit(1);
+    }
+
+    return l_process;
 }
